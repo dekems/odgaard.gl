7661a3854cfbdee1ec48d79a9dae80e0c13dad24
dekems <nicolai@odgaard.gl>
2025-10-07 12:12:52 -0100

    ver. 0.1a
    Første version med logo og kontaktinformation.

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const yearSpan = document.querySelector('[data-year]');
+  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+});
